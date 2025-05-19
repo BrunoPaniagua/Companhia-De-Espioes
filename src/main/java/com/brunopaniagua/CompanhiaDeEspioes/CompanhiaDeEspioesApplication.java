3f90bdf4d5750cec6eb9422f158a150c9ca1a048
Bruno Paniagua <brunopaniaguaoliveira@gmail.com>
2025-05-18 21:18:43 -0300

    Criando o projeto

---
+++ src/main/java/com/brunopaniagua/CompanhiaDeEspioes/CompanhiaDeEspioesApplication.java
@@ -0,0 +1,13 @@
+package com.brunopaniagua.CompanhiaDeEspioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanhiaDeEspioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanhiaDeEspioesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunopaniagua/CompanhiaDeEspioes/CompanhiaDeEspioesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunopaniagua.CompanhiaDeEspioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanhiaDeEspioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
